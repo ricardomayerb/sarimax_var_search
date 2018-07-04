8a7aab052de0db26afff70cf15d825720f30d6d4
ricardomayerb <ricardomayerb@gmail.com>
2018-07-04 16:45:39 -0400

    to merge

--- R/use_new_get_arima_results.R
+++ R/use_new_get_arima_results.R
@@ -1,6 +1,6 @@
 source('./R/utils_av.R')
 
-country_name <- "Argentina"
+country_name <- "Ecuador"
 
 # this is the "stata" option (demetra + always include a constant even if D+d = 2). Approx 20 sec in my PC. ARG 30 sec
 tic()
