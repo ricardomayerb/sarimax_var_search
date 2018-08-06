035e16c6b2c7edeee1b2dd432f2bb22391e8ffed
Ricardo Mayer <ricardomayerb@gmail.com>
2018-08-06 09:34:06 -0400

    casa

---
+++ R/comparing_arimas.R
@@ -0,0 +1,193 @@
+source('./R/utils_av_ricardo.R')
+
+seas_yr_growth <- function(data_ts, freq = 4 , is_log = TRUE, 
+                           do_just_log = FALSE) {
+  
+  seas_gr <- tsibble::as_tsibble(data_ts)
+  
+  if (is_log) {
+    seas_gr  <- seas_gr %>% 
+      mutate(difflog_gr =  100 * difference(value, lag = freq))
+    
+    year_gr <- seas_gr %>% index_by(year_index = year(index)) %>%
+      summarise(y_logdiff = mean(difflog_gr, na.rm = TRUE)) 
+    
+    if (freq == 12) {
+      quarter_gr <- seas_gr %>% index_by(quarter_index = yearquarter(index)) %>%
+        summarise(q_logdiff = mean(difflog_gr, na.rm = TRUE)) 
+    }
+    
+    if (! do_just_log) {
+      seas_gr  <-seas_gr  %>% 
+        mutate(gr = 100 * difference(exp(value), lag = freq) / 
+                 dplyr::lag(exp(value), n = freq))
+      
+      year_gr <- seas_gr %>% index_by(year_index = year(index)) %>%
+        summarise(y_logdiff = mean(difflog_gr, na.rm = TRUE),
+                  y_ave_gr = mean(gr, na.rm = TRUE),
+                  y_rgdp = sum(exp(value), na.rm = TRUE))  %>%
+        mutate(y_tot_gr = 100*difference(y_rgdp)/lag(y_rgdp)) 
+      
+      if (freq == 12) {
+        quarter_gr <- seas_gr %>% index_by(quarter_index = yearquarter(index)) %>%
+          summarise(q_logdiff = mean(difflog_gr, na.rm = TRUE),
+                    q_ave_gr = mean(gr, na.rm = TRUE),
+                    q_rgdp = sum(exp(value), na.rm = TRUE))  %>%
+          mutate(q_tot_gr = 100*difference(q_rgdp, lag = 4)/lag(q_rgdp, n = 4))
+        
+      }
+    }
+    
+  } else {
+    seas_gr  <-seas_gr  %>% 
+      mutate(gr = 100 * difference(value, lag = freq) / 
+               dplyr::lag(value, n = freq))
+    
+    year_gr <- seas_gr %>% index_by(year_index = year(index)) %>% 
+      summarise(y_ave_gr = mean(gr, na.rm = TRUE),
+                y_rgdp = sum(value)) %>%
+      mutate(y_tot_gr = 100*difference(y_rgdp)/lag(y_rgdp))
+    
+    if (freq == 12) {
+      quarter_gr <- seas_gr %>% index_by(quarter_index = yearquarter(index)) %>% 
+        summarise(q_ave_gr = mean(gr, na.rm = TRUE),
+                  q_rgdp = sum(value)) %>%
+        mutate(q_tot_gr = 100*difference(q_rgdp, lag = 4)/lag(q_rgdp, n = 4))
+    }
+    
+  }
+  
+  
+  if (freq == 4) {
+    return(list(seasonal_growth = seas_gr, year_growth = year_gr))
+  }
+  
+  if (freq == 12) {
+    return(list(seasonal_growth = seas_gr, year_growth = year_gr,
+                quarter_growth = quarter_gr))
+  }
+  
+  
+  
+}
+
+print_growths <- function(seas_and_yr_gr) {
+  seas_gr <- seas_and_yr_gr[["seasonal_growth"]]
+  year_gr <- seas_and_yr_gr[["year_growth"]]
+  quarter_gr <- seas_and_yr_gr[["quarter_growth"]]
+  
+  seas <- knitr::kable(tail(as_tibble(seas_gr) %>% select(-value), n = 8), 
+                       digits = 1, align = 'lcc')
+  
+  yr <- knitr::kable(tail(as_tibble(year_gr) %>% select(-y_rgdp), n = 5),
+                     digits = 1, align = 'lccc')  
+  
+  if (is.null(quarter_gr)) {
+    
+    walk(list(seas, yr), print)
+    
+  } else {
+    
+    qtr <- knitr::kable(tail(as_tibble(quarter_gr) %>% select(-q_rgdp), n = 5),
+                        digits = 1, align = 'lccc') 
+    
+    walk(list(seas, yr, qtr), print)
+    
+  }
+  
+}
+
+country_name <- "Argentina"
+arima_rds_path = "data/sarimax_objects_"
+
+arima_res_suffix <- "_dm_s"
+rds_file_name = paste0(arima_rds_path, country_name, arima_res_suffix, ".rds")
+dm_s <- readRDS(file = rds_file_name)
+
+arima_res_suffix <- "_dm_s_fsv"
+rds_file_name = paste0(arima_rds_path, country_name, arima_res_suffix, ".rds")
+dm_s_fsv <- readRDS(file = rds_file_name)
+
+arima_res_suffix <- "_dm_r"
+rds_file_name = paste0(arima_rds_path, country_name, arima_res_suffix, ".rds")
+dm_r <- readRDS(file = rds_file_name)
+
+arima_res_suffix <- "_dm_r_fsv"
+rds_file_name = paste0(arima_rds_path, country_name, arima_res_suffix, ".rds")
+dm_r_fsv <- readRDS(file = rds_file_name)
+
+arima_res_suffix <- "_auto"
+rds_file_name = paste0(arima_rds_path, country_name, arima_res_suffix, ".rds")
+auto <- readRDS(file = rds_file_name)
+
+arima_res_suffix <- "_auto_fsv"
+rds_file_name = paste0(arima_rds_path, country_name, arima_res_suffix, ".rds")
+auto_fsv <- readRDS(file = rds_file_name)
+
+
+data_and_fc_unc_dm_s <- dm_s$rgdp_data_and_uncond_fc
+data_and_fc_unc_dm_r <- dm_r$rgdp_data_and_uncond_fc
+data_and_fc_unc_auto <- auto$rgdp_data_and_uncond_fc
+
+autoplot(data_and_fc_unc_dm_s) + 
+  autolayer(data_and_fc_unc_dm_r) +
+  autolayer(data_and_fc_unc_auto) 
+
+yoy_data_and_fc_unc_dm_s <- dm_s$yoy_rgdp_data_and_uncond_fc
+yoy_data_and_fc_unc_dm_r <- dm_r$yoy_rgdp_data_and_uncond_fc
+yoy_data_and_fc_unc_auto <- auto$yoy_rgdp_data_and_uncond_fc
+
+autoplot(yoy_data_and_fc_unc_dm_s) + 
+  autolayer(yoy_data_and_fc_unc_dm_r) +
+  autolayer(yoy_data_and_fc_unc_auto) 
+
+gr_qrt_data_and_fc_unc_dm_s <- seas_yr_growth(data_and_fc_unc_dm_s)[["seasonal_growth"]]
+gr_qrt_data_and_fc_unc_dm_r <- seas_yr_growth(data_and_fc_unc_dm_r)[["seasonal_growth"]]
+gr_qrt_data_and_fc_unc_auto <- seas_yr_growth(data_and_fc_unc_auto)[["seasonal_growth"]]
+
+q_2018_logic <- year(gr_qrt_data_and_fc_unc_dm_r$index) >= 2018 
+q_2019_logic <- year(gr_qrt_data_and_fc_unc_dm_r$index) <= 2019 
+q_2018_2019_logic <- q_2018_logic & q_2019_logic
+
+gr_qrt_data_and_fc_unc_dm_s_2018_2019 <- gr_qrt_data_and_fc_unc_dm_s %>% filter(q_2018_2019_logic)
+gr_qrt_data_and_fc_unc_dm_r_2018_2019 <- gr_qrt_data_and_fc_unc_dm_r %>% filter(q_2018_2019_logic)
+gr_qrt_data_and_fc_unc_auto_2018_2019 <- gr_qrt_data_and_fc_unc_auto %>% filter(q_2018_2019_logic)
+
+
+gr_yr_data_and_fc_unc_dm_r <- seas_yr_growth(data_and_fc_unc_dm_r)[["year_growth"]] %>% filter(year_index <= 2019)
+gr_yr_data_and_fc_unc_dm_s <- seas_yr_growth(data_and_fc_unc_dm_s)[["year_growth"]] %>% filter(year_index <= 2019)
+gr_yr_data_and_fc_unc_auto <- seas_yr_growth(data_and_fc_unc_auto)[["year_growth"]] %>% filter(year_index <= 2019)
+
+y_2018_logic <- gr_yr_data_and_fc_unc_dm_r$year_index >= 2018 
+y_2019_logic <- gr_yr_data_and_fc_unc_dm_r$year_index <= 2019 
+y_2018_2019_logic <- y_2018_logic & y_2019_logic
+
+
+gr_yr_data_and_fc_unc_dm_s_2018_2019 <- gr_yr_data_and_fc_unc_dm_s %>% filter(y_2018_2019_logic)
+gr_yr_data_and_fc_unc_dm_r_2018_2019 <- gr_yr_data_and_fc_unc_dm_r %>% filter(y_2018_2019_logic)
+gr_yr_data_and_fc_unc_auto_2018_2019 <- gr_yr_data_and_fc_unc_auto %>% filter(y_2018_2019_logic)
+
+
+
+gr_qrt_201819_3_arimas <- dplyr::left_join(gr_qrt_data_and_fc_unc_dm_s, 
+                                           gr_qrt_data_and_fc_unc_dm_r,
+                                           by = "index", 
+                                           suffix = c("_dm_s", "_dm_r")) %>% 
+  left_join(gr_qrt_data_and_fc_unc_auto, by = "index") %>% 
+  select(index, gr_dm_s, gr_dm_r, gr) %>% 
+  filter(year(index) >= 2018, year(index) <= 2019)
+
+
+
+gr_yr_201819_3_arimas <- dplyr::left_join(gr_yr_data_and_fc_unc_dm_s, gr_yr_data_and_fc_unc_dm_r,
+                                   by = "year_index", suffix = c("_dm_s", "_dm_r")) %>% 
+  left_join(gr_yr_data_and_fc_unc_auto, by = "year_index") %>% 
+  select(year_index, y_ave_gr_dm_s, y_ave_gr_dm_r, y_ave_gr) %>% 
+  filter(year_index >= 2018, year_index <= 2019)
+
+gr_qrt_201819_3_arimas
+
+gr_yr_201819_3_arimas  
+
+
+
